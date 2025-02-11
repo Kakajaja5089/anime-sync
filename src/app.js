c3ed5080d174469c1fb285ced6ad19eaab68cb5c
Kakajaja5089 <kakajaja5089@gmail.com>
2025-02-11 13:03:54 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv");
 
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+// Import routes
+const mappingsRoutes = require("./routes/mappings");
+const authRoutes = require("./routes/auth");
+const syncRoutes = require("./routes/sync");
+
+// Use routes
+app.use("/mappings", mappingsRoutes);
+app.use("/auth", authRoutes);
+app.use("/sync", syncRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
