8dfb20ea983439ac61b0306460c3d5dfc1e1ac5d
Kakajaja5089 <kakajaja5089@gmail.com>
2025-02-11 13:07:52 +0100

    Update sync.js

--- src/services/sync.js
+++ src/services/sync.js
@@ -1,1 +1,22 @@
+const axios = require("axios");
 
+async function syncUserAnimeList(userId) {
+    // Fetch from Anilist
+    const anilistRes = await axios.post("https://graphql.anilist.co", {
+        query: `
+        query ($userId: Int) {
+            MediaListCollection(userId: $userId, type: ANIME) {
+                lists {
+                    entries { mediaId status score progress }
+                }
+            }
+        }`,
+        variables: { userId }
+    });
+
+    const anilistList = anilistRes.data.data.MediaListCollection.lists.flatMap(l => l.entries);
+
+    return { anilist: anilistList };
+}
+
+module.exports = { syncUserAnimeList };
