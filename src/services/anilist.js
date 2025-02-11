4cd45638168e137fd57393b7428a8125584a7cbe
Kakajaja5089 <kakajaja5089@gmail.com>
2025-02-11 13:04:34 +0100

    Update anilist.js

--- src/services/anilist.js
+++ src/services/anilist.js
@@ -1,1 +1,29 @@
+const axios = require("axios");
 
+async function getAnimeMappings(anilistId) {
+    const query = `
+        query ($id: Int) {
+            Media(id: $id, type: ANIME) {
+                id
+                title { romaji english native }
+                externalLinks { id site url }
+            }
+        }`;
+
+    const response = await axios.post("https://graphql.anilist.co", {
+        query,
+        variables: { id: anilistId }
+    });
+
+    const data = response.data.data.Media;
+    return {
+        anilist_id: data.id,
+        titles: data.title,
+        mappings: data.externalLinks.reduce((acc, link) => {
+            acc[link.site.toLowerCase()] = link.id;
+            return acc;
+        }, {})
+    };
+}
+
+module.exports = { getAnimeMappings };
